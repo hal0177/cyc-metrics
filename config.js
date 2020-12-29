ff0e57df34397cde20608fff8d28a3a9d4102cee
Paddy Curé <paddy.cure@gmail.com>
2020-12-29 18:21:23 +0000

    gets latest bsc block height

--- config.js
+++ config.js
@@ -1,19 +1,10 @@
 module.exports = {
-    blockchain: {
-        tokenAbi: require("./abi/token.json"),
-        exchangeAbi: require("./abi/exchange.json"),
-        providerType: "http",
-        providerPath: "https://bsc-dataseed1.binance.org/",
-        chainId: 32659,
-        bscId: 56,
-        startHeight: 2545943,
-        cycle: 100,
-        gasPrice: "1", // gwei
-        privateKey: "your private key",
-        address: "0x0F61a9c5C2e5af0Fbd3BDbd75300ACf9c657C5AD".toLocaleLowerCase(), // my public address
-        tokenAddress: "0x4028433877f9c14764eb93d0bb6570573da2726f".toLocaleLowerCase(), // cyc token address
-        exchangeAddress: "0x049DdC3CD20aC7a2F6C867680F7E21De70ACA9C3".toLocaleLowerCase(), // fsn/any exchange address
-    },
-    charge: 0.004,
-    delay: 30
+  blockchain: {
+      tokenAbi: require("./abi/CYC.json"),
+      providerPath: "https://bsc-dataseed1.binance.org/",
+      chainId: 56,
+      startHeight: "start height",
+      address: "0x0F61a9c5C2e5af0Fbd3BDbd75300ACf9c657C5AD".toLocaleLowerCase(), // my public address
+      tokenAddress: "0x4028433877f9c14764eb93d0bb6570573da2726f".toLocaleLowerCase(), // cyc token address
+  },
 }
